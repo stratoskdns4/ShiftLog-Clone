f6fb630560cea960f068f3fc427a09431313a5a9
Stratos Kydonas <stratoskdns4@gmail.com>
2023-06-29 11:01:06 +0300

    Refactored app into a class

--- app.py
+++ app.py
@@ -1,17 +1,45 @@
 import tkinter as tk
-from tkinter import ttk
-
 from applog import setup_logging
-
 from frames.main_frame import MainFrame
 from frames.login_frame import LoginFrame
-
 from login_controller import LoginController
+
 
 DEBUG = True
 
-logger = setup_logging()
-logger.debug('app.py: STARTING')
+
+class App:
+
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Καταγραφη!")
+        self.window.geometry("1200x700")
+        
+        self.main_frame = None
+        self.login_frame = None
+
+    def go_to_main_view(self):
+        if self.login_frame is not None:
+            self.login_frame.destroy()
+        self.main_frame = MainFrame(self.window)
+        self.main_frame.pack(fill=tk.BOTH, expand=True)
+
+    def go_to_login_view(self):
+        if self.main_frame is not None:
+            self.main_frame.destroy()
+        self.login_frame = LoginFrame(self.window)
+        self.login_frame.pack(anchor=tk.CENTER, expand=True)
+
+    def run(self):
+        self.go_to_login_view()
+        lc = LoginController(self.go_to_main_view, self.go_to_login_view)
+
+        if DEBUG:
+            lc.do_login('aaaa', 'bbbb')
+        
+        logger.warning('mainloop of app')
+        self.window.mainloop()
+    
 
 
 if __name__ == '__main__':
@@ -22,35 +50,9 @@ if __name__ == '__main__':
         from ctypes import windll
         windll.shcore.SetProcessDpiAwareness(1)
 
-    logger.warning('mainloop of app')
-    window = tk.Tk()
-    window.title("Καταγραφη!")
-    window.geometry("1200x700")
+        
+    logger = setup_logging()
+    logger.debug('app.py: STARTING')
 
-    # window.tk.call('tk', 'scaling', 2.0)
-    
-    main_frame = None
-    login_frame = None
-
-    def go_to_main_view():
-        global main_frame
-        if login_frame is not None:
-            login_frame.destroy()
-        main_frame = MainFrame(window)
-        main_frame.pack(fill=tk.BOTH, expand=True)
-
-    def go_to_login_view():
-        global login_frame
-        if main_frame is not None:
-            main_frame.destroy()
-        login_frame = LoginFrame(window)
-        login_frame.pack(anchor=tk.CENTER, expand=True)
-
-
-    go_to_login_view()
-    lc = LoginController(go_to_main_view, go_to_login_view)
-
-    if DEBUG:
-        lc.do_login('aaaa', 'bbbb')
-
-    window.mainloop()
+    app = App()
+    app.run()
